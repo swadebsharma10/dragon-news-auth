f7b5f8bd8b26de178b72bd71f2a743e2321b199d
SwadebDeb sharma <swadebriva2012@gmail.com>
2024-09-23 00:24:04 +0600

    added left side navigation

--- src/components/SideNav/LeftSideNav.jsx
+++ src/components/SideNav/LeftSideNav.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+      const [categories, setCategories] = useState([]);
+
+      useEffect(()=>{
+            fetch('/public/data/categories.json')
+            .then(res => res.json())
+            .then(data =>{
+                  console.log(data);
+                  setCategories(data)
+            })
+      }, [])
+
       return (
             <div>
-                 <h2 className="text-2xl">Left side nav</h2> 
+                 <h2 className="font-bold mb-6">All Categories</h2> 
+                 {
+                  categories.map(category => <Link 
+                        key={category.id}
+                  className="block mb-4"
+                  to={`/category/${category.id}`}>
+                  <button className="btn btn-sm btn-wide">{category.name}</button>
+                  </Link>
+                  
+                  )
+                 }
             </div>
       );
 };
